2a613027acf54c385579944a2f452bcd351325a0
Luxshan-Partheepan-ICD2O1-01 <luxshan.partheepan@student.tdsb.on.ca>
2024-11-04 22:19:31 -0500

    Update PP1_3.py

--- PP1_3.py
+++ PP1_3.py
@@ -4,18 +4,37 @@ def q1():
   #Write Assignment code here
   #Remember your code must be indented once
 
-  " * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *"
+    print(" * * * * * *")
+    print("* * * * * *")
+    print(" * * * * * *")
+    print("* * * * * *")
+    print(" * * * * * *")
+    print("* * * * * *")
 
 
-  "* * * * *\n*        *\n*        *\n*        *\n* * * * *"
 
-  "* * * * * *\n*       *\n*     *\n*   *\n* *\n* "
+    print("* * * * *")
+    print("*       *")
+    print("*       *")
+    print("*       *")
+    print("* * * * *")
+
+
+
+    print("* * * * * *")
+    print(" *       *")
+    print("  *     *")
+    print("   *   *")
+    print("    * *")
+    print("     *")
+    
+
 
 
 def q2():
 
- print("Hello", end=" ")
- print("world")
+    print("Hello", end=" ") 
+    print("World")
   
 def q3():
   #Write Assignment code here
